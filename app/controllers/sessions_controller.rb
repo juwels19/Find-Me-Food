8a7861b54e1eb35faeca7fd9f75d231a718a3337
Julian C <julian.curalli27@gmail.com>
2020-08-19 00:53:35 -0400

    Login system working

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to users_url, notice: "Successfully logged in!"
+    else
+      flash.now[:alert] = "Email or password is invalid. Please try again."
+      render "new"
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to login_url, notice: "Successfully logged out!"
+  end
+end
